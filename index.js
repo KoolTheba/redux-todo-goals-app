3cd059152aa22f7c590729ff04853de43fc9192d
Theba Gomez <gomez.teba@gmail.com>
2020-08-20 12:31:27 +0200

    feat(state): Added subscribe method

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 function createStore () {
     let state
+    let listeners = []
 
     const getState = () => state
 
+    const subscribe = (listener) => {
+        listeners.push(listener)
+        return () => {
+            listeners = listeners.filter(l => l !== listener)
+        }
+    }
+
     return {
-        getState
+        getState,
+        subscribe
     }
 }
