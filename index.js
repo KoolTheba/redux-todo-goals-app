83ab7e1aa3d4c78b38b0788a10c6951e5f178327
Theba Gomez <gomez.teba@gmail.com>
2020-08-20 16:39:39 +0200

    feat(store): Added handling examples

--- index.js
+++ index.js
@@ -65,4 +65,66 @@ function app (state = {}, action){
     }
 }
 
-// const store = createStore(app)
+const store = createStore(app)
+
+store.subscribe(() => {
+    console.log('The new state is: ', store.getState())
+})
+
+store.dispatch({
+    type: ADD_TODO,
+    todo: {
+        id: 0,
+        name: 'Walk the dog',
+        complete: false,
+    }
+})
+
+store.dispatch({
+    type: ADD_TODO,
+    todo: {
+        id: 1,
+        name: 'Wash the car',
+        complete: false,
+    }
+})
+
+store.dispatch({
+    type: ADD_TODO,
+    todo: {
+        id: 2,
+        name: 'Go to the gym',
+        complete: true,
+    }
+})
+
+store.dispatch({
+    type: REMOVE_TODO,
+    id: 1
+})
+
+store.dispatch({
+    type: TOGGLE_TODO,
+    id: 0
+})
+
+store.dispatch({
+    type: ADD_GOAL,
+    goal: {
+        id: 0,
+        name: 'Learn Redux'
+    }
+})
+
+store.dispatch({
+    type: ADD_GOAL,
+    goal: {
+        id: 1,
+        name: 'Lose 20 pounds'
+    }
+})
+
+store.dispatch({
+    type: REMOVE_GOAL,
+    id: 0
+})
