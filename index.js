2d02ddd6a1919e69690ded51fc4b02d6657a2f81
Theba Gomez <gomez.teba@gmail.com>
2020-08-20 16:37:47 +0200

    feat(store): Leveraged constants

--- index.js
+++ index.js
@@ -1,36 +1,4 @@
-function todos (state = [], action) {
-    switch(action.type){
-        case 'ADD_TODO':
-            return state.concat([action.todo])
-        case 'REMOVE_TODO':
-            return state.filter(todo => todo.id !== action.id)
-        case 'TOGGLE:TODO':
-            return state.map(todo => todo.id !== action.id ? todo : 
-                Object.assign({}, todo, {complete: !todo.complete})    
-            )
-        default:
-            return state
-    }
-}
-
-function goals (state = [], action){
-    switch(action.type){
-        case 'ADD_GOAL':
-            return state.concat([action.goal])
-        case 'REMOVE_GOAL':
-            return state.filter(goal => goal.id !== action.id)
-        default:
-            return state
-    }
-}
-
-function app (state = {}, action){
-    return {
-        todos: todos(state.todos, action),
-        goals: goals(state.goals, action)
-    }
-}
-
+// Library code
 function createStore (reducer) {
     let state
     let listeners = []
@@ -54,6 +22,46 @@ function createStore (reducer) {
         getState,
         subscribe,
         dispatch
+    }
+}
+
+// App code
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
+function todos (state = [], action) {
+    switch(action.type){
+        case ADD_TODO:
+            return state.concat([action.todo])
+        case REMOVE_TODO:
+            return state.filter(todo => todo.id !== action.id)
+        case TOGGLE_TODO:
+            return state.map(todo => todo.id !== action.id ? todo : 
+                Object.assign({}, todo, {complete: !todo.complete})    
+            )
+        default:
+            return state
+    }
+}
+
+function goals (state = [], action){
+    switch(action.type){
+        case ADD_GOAL:
+            return state.concat([action.goal])
+        case REMOVE_GOAL:
+            return state.filter(goal => goal.id !== action.id)
+        default:
+            return state
+    }
+}
+
+function app (state = {}, action){
+    return {
+        todos: todos(state.todos, action),
+        goals: goals(state.goals, action)
     }
 }
 
