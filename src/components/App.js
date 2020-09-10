db534dbd84b359fb761a871f3dce49280fef73c6
Theba Gomez <gomez.teba@gmail.com>
2020-09-10 12:50:24 +0200

    feat(UI): added App component file

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import * as React from 'react'
+import ConnectedTodos from './Todos'
+import ConnectedGoals from './Goals'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
 
-export default function App () {
-  return (
-    <div>
-      Goals - Todos
-    </div>
-  )
+class App extends React.Component {
+
+  componentDidMount () {
+      const { dispatch } = this.props
+      dispatch(handleInitialData())
+  }
+
+  render() {
+      const { loading } = this.props
+
+      if(loading){
+          return <h3>Loading...</h3>
+      }
+
+      return (
+          <div>
+          <ConnectedTodos/>
+          <ConnectedGoals/>   
+          </div>
+      )
+  }
 }
+
+export default connect((state) => ({
+  loading: state.loading
+}))(App)
