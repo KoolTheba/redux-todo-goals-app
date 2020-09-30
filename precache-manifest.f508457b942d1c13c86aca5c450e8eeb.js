e68fe1e7e7216d7f5152332db36baee0a3caec21
Theba Gomez <gomez.teba@gmail.com>
2020-09-30 11:54:59 +0200

    Updates

---
+++ precache-manifest.f508457b942d1c13c86aca5c450e8eeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd7d07168afa2d386fccc7f0a72633b9",
+    "url": "/redux-todo-goals-app/index.html"
+  },
+  {
+    "revision": "57710bbe1e71761947aa",
+    "url": "/redux-todo-goals-app/static/css/main.f136409c.chunk.css"
+  },
+  {
+    "revision": "6098adb3496e0fc23c70",
+    "url": "/redux-todo-goals-app/static/js/2.0a63b704.chunk.js"
+  },
+  {
+    "revision": "9f90cde4396c52b73cc0a6bcd8267b7b",
+    "url": "/redux-todo-goals-app/static/js/2.0a63b704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57710bbe1e71761947aa",
+    "url": "/redux-todo-goals-app/static/js/main.f93c9a41.chunk.js"
+  },
+  {
+    "revision": "5dc6c59449cd0f7c1dcb",
+    "url": "/redux-todo-goals-app/static/js/runtime-main.c0a7bed8.js"
+  }
+]);
